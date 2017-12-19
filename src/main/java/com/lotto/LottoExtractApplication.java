b35ca48dc109ccab136e7e277a7b2141a36c6d38
장재훈 <1700130@partner.skcc.com>
2017-12-19 16:58:13 +0900

    Lotto project start

---
+++ src/main/java/com/lotto/LottoExtractApplication.java
@@ -0,0 +1,12 @@
+package com.lotto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LottoExtractApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LottoExtractApplication.class, args);
+	}
+}
---
+++ src/main/java/com/lotto/extract/controller/ExtractController.java
@@ -0,0 +1,14 @@
+package com.lotto.extract.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/extract")
+public class ExtractController {
+
+	@RequestMapping("/getNumber")
+	public void extractNumber() {
+		
+	}
+}
